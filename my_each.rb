e2c310876fa6462b18ab8152955f5d15feed3836
Nataliya R <n.rudenko@gmail.com>
2019-02-19 02:17:59 +0000

    Automatically backed up by Learn

---
+++ my_each.rb
@@ -0,0 +1,16 @@
+def my_each (name)
+  while name < 3
+  "The current name"
+  name += 1
+end
+end
+
+def my_each(words)
+  i = 0 
+  while i < words.length
+  yield words[i]
+  i = i + 1 
+end 
+ words
+end
+  
